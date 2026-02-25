e1a91460466f13170e46babfe73670487b6a6360
Santiago77 <santiagopovedam@gmail.com>
2026-02-24 20:54:56 -0500

    Add files via upload

---
+++ Numeros1Al10.java
@@ -0,0 +1,12 @@
+public class Numeros1Al10 {
+
+        public static void numeros() {
+            for (int i = 1; i <= 10; i ++) {
+                System.out.println(i);
+            }
+        }
+
+    public static void main(String[] args) {
+        numeros();
+    }
+}
